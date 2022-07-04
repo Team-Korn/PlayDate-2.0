edb3e95ddd794ded099bc02ba738bbadaadfb6c0
Katie Assoian <ktassoian@gmail.com>
2022-07-04 15:59:54 -0400

    file structure, add redux, change ReadME

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom/src';
+import '../public/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import history from './history';
+import store from './store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
+    {/* <React.StrictMode> */}
+    {/*dont know if needed ^^^^  */}
+    <Router history={history}>
+      <App />
+    </Router>
+    {/* </React.StrictMode> */}
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
