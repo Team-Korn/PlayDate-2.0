9d66414bfe30d30f6eefdfd81ed02cf284a2e2d3
Ji Kim <jiyoung.kimmy@gmail.com>
2022-07-05 07:42:26 -0400

    clearing up unnecessary code in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 // ------------------ FIREBASE SDK --------------------------------
 import firebase from 'firebase/app';
@@ -26,44 +27,8 @@ firebase.initializeApp({
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
-// function App() {
-//   return <div className="App">
-//     Welcome to PlayDate!</div>;
-// }
-
-// ------- ADDING A COMPONENT ---------------------------------
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Looking for Similar 4 Legged Playmates</h1>
-      </div>
-    );
-  }
+function App() {
+  return <div className="App">Welcome to PlayDate!</div>;
 }
 
 export default App;
-
-/*
-
-// --------------- FIREBASE INITIAL ------------------
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA7BGrGY74CDLLDfTBakn8s-5BpXVKRkpA",
-  authDomain: "playdate-9bf9a.firebaseapp.com",
-  projectId: "playdate-9bf9a",
-  storageBucket: "playdate-9bf9a.appspot.com",
-  messagingSenderId: "211081650181",
-  appId: "1:211081650181:web:4ffb86866e49b88122bc6c"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-*/
