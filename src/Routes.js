b3b7cf0e79dab5efa182dfe7c370648b9521fe6a
Katie Assoian <ktassoian@gmail.com>
2022-07-04 22:13:50 -0400

    feat(setup): fix bug with router

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,12 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import HomePage from './components/HomePage';
 
 const Routes = () => {
   return (
     <div>
       <Switch>
-        <Route>...</Route>
+        <Route path="/home" component={HomePage} />
       </Switch>
     </div>
   );
