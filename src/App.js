d862daea4b6fdad814e0e7f95be54475cde91070
Ji Kim <jiyoung.kimmy@gmail.com>
2022-07-05 08:02:35 -0400

    npm start shows App.js file on local host 3000

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <div className="App">Welcome to PlayDate!</div>;
+  return (
+    <div className="App">
+      Welcome to PlayDate! Where you Find Four-Legged Friends
+    </div>
+  );
 }
 
 export default App;
