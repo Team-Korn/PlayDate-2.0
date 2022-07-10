e68de5fe46296d01e0fb907148eb875dbe2e5fe8
Katie Assoian <ktassoian@gmail.com>
2022-07-10 14:44:40 -0400

    message icon route

--- src/components/SwipeButton.js
+++ src/components/SwipeButton.js
@@ -6,6 +6,7 @@ import StarRateIcon from '@material-ui/icons/StarRate';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import IconButton from '@material-ui/core/IconButton';
 import ChatIcon from '@mui/icons-material/Chat';
+import { Link } from 'react-router-dom';
 
 function SwipeButton() {
   return (
@@ -22,9 +23,11 @@ function SwipeButton() {
       <IconButton className="swipeButtons__right">
         <FavoriteIcon fontSize="large" />
       </IconButton>
-      <IconButton className="swipeButtons__message">
-        <ChatIcon />
-      </IconButton>
+      <Link to="/chat">
+        <IconButton className="swipeButtons__message">
+          <ChatIcon />
+        </IconButton>
+      </Link>
     </div>
   );
 }
