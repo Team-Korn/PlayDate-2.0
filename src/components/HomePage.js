c02f152c01af8556ef3916d60b7ca6d270ee7ac1
Ji Kim <jiyoung.kimmy@gmail.com>
2022-07-11 17:07:05 -0400

    cannot go into homepage unless logged in

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SwipeCard from './SwipeCard';
 import SwipeButton from './SwipeButton';
+import { useNavigate } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../Auth';
 
 const HomePage = () => {
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    } else {
+      return;
+    }
+  }, [user, loading, navigate]);
+
   return (
     <div>
       <SwipeCard />
