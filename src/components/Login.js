a668e17bb6ab72c112dce569729d40748ac164d9
Katie Assoian <ktassoian@gmail.com>
2022-07-11 12:08:40 -0400

    all bottom btns clickable

--- src/components/Login.js
+++ src/components/Login.js
@@ -24,7 +24,7 @@ function Login() {
       // maybe trigger a loading screen
       return;
     }
-    if (user) navigate('/dashboard');
+    if (user) navigate('/home');
   }, [user, loading]);
   return (
     <div className="login">
