a5e45dccecada8983a3cca3ee123ec83056d0fbc
Nyjia Williams <nyjiawilliams@gmail.com>
2022-07-13 22:15:52 -0500

    accessing current user/dog data in settings

--- src/components/user/DogSettings.js
+++ src/components/user/DogSettings.js
@@ -1,52 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { app, db } from '../../config/fbConfig';
+import { getAuth } from 'firebase/auth';
 import './Settings.css'
 
 const DogSettings = () => {
-  return (
-    <div className="container">
-      <form action="action_page.php">
-        <div className="row">
-          <div className="col-25">
-            <label htmlFor="name">Name</label>
+  const auth = getAuth(app);
+  const user = auth.currentUser;
+  const [dogs, setDogs] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'dogs'));
+        const dogData = [];
+        querySnapshot.forEach((doc) => {
+          dogData.push(doc.data());
+        });
+        setDogs(dogData);
+      } catch (err) {
+        collection.log(err, 'who let the dogs out?');
+      }
+    })();
+  }, []);
+
+  const currDog = dogs.filter((dog) => {
+    return dog.ownerId === user.uid;
+  });
+
+  if(!dogs[0]) {
+    console.log('No Dogs Here!');
+  } else {
+    return (
+      <div className="container">
+        <form className='settings'>
+          <div className="row">
+            <div className="col-25">
+              <label htmlFor="name">Name</label>
+            </div>
+            <div className="col-75">
+              <input type="text"  name="name" placeholder={currDog[0].name}/>
+            </div>
           </div>
-          <div className="col-75">
-            <input type="text" id="name" name="name" placeholder="Your name..."/>
+          <div className="row">
+            <div className="col-25">
+              <label htmlFor="breed">Breed</label>
+            </div>
+            <div className="col-75">
+              <input type="text" name="breed" placeholder={currDog[0].breed}/>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-25">
-            <label htmlFor="breed">Breed</label>
+          <div className="row">
+            <div className="col-25">
+              <label htmlFor="size">Size</label>
+            </div>
+            <div className="col-75">
+              <select name="size">
+                <option value="Small">Small</option>
+                <option value="Medium">Medium</option>
+                <option value="Large">Large</option>
+              </select>
+            </div>
           </div>
-          <div className="col-75">
-            <input type="text" id="breed" name="breed" placeholder="What type of furry friend do you have.."/>
+          <div className="row">
+            <div className="col-25">
+              <label htmlFor="about-me">About me</label>
+            </div>
+            <div className="col-75">
+              <textarea id="about-me" name="subject" placeholder={currDog[0].bio} ></textarea>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-25">
-            <label htmlFor="size">Size</label>
+          <div className="row">
+            <input type="submit" value="Submit"/>
           </div>
-          <div className="col-75">
-            <select id="size" name="size">
-              <option value="Small">Small</option>
-              <option value="Medium">Medium</option>
-              <option value="Large">Large</option>
-            </select>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-25">
-            <label htmlFor="about-me">About me</label>
-          </div>
-          <div className="col-75">
-            <textarea id="subject" name="subject" placeholder="Write something.." ></textarea>
-          </div>
-        </div>
-        <div className="row">
-          <input type="submit" value="Submit"/>
-        </div>
-      </form>
-    </div>
-  )
+        </form>
+      </div>
+    )
+  }
 }
 
 export default DogSettings
--- src/components/user/UserSettings.js
+++ src/components/user/UserSettings.js
@@ -1,48 +1,95 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { app, db } from '../../config/fbConfig';
+import { getAuth } from 'firebase/auth';
 import './Settings.css'
 
 const UserSettings = () => {
-  return (
-    <div className="container">
-    <form action="action_page.php">
-      <div className="row">
-        <div className="col-25">
-          <label htmlFor="name">Name</label>
+  //get current users uid
+  const auth = getAuth(app);
+  const authUser = auth.currentUser;
+  // console.log("THIS IS THE USER: ", authUser)
+
+  //grab all users from db
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'users'));
+        const userData = [];
+        querySnapshot.forEach((doc) => {
+          userData.push(doc.data());
+        });
+
+        setUsers(userData);
+      } catch (err) {
+        console.log(err, 'who let the dogs out?');
+      }
+    })();
+  }, []);
+
+  // console.log('USERS: ', users)
+
+  //find the current users object
+  const currUser = users.filter(user => {
+    return user.uid === authUser.uid
+  })
+
+  console.log("CURRENT USER DATA: ", currUser);
+
+  if(!currUser[0]) {
+    console.log('NO USER FOUND!');
+  } else {
+    return (
+      <div className="container">
+      <form className='settings'>
+        <div className="row">
+          <div className="col-25">
+            <label htmlFor="name">Name</label>
+          </div>
+          <div className="col-75">
+            <input type="text" name="name" placeholder={currUser[0].name}/>
+          </div>
         </div>
-        <div className="col-75">
-          <input type="text" id="name" name="name" placeholder="Your name.."/>
+        <div className="row">
+          <div className="col-25">
+            <label htmlFor="email">Email</label>
+          </div>
+          <div className="col-75">
+            <input type="text" name="email" placeholder={currUser[0].email}/>
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-25">
-          <label htmlFor="email">Email</label>
+        <div className="row">
+          <div className="col-25">
+            <label htmlFor="city">City</label>
+          </div>
+          <div className="col-75">
+            <input type="text" name="city" placeholder={currUser[0].city}/>
+          </div>
         </div>
-        <div className="col-75">
-          <input type="text" id="lname" name="lastname" placeholder="Your email.."/>
+        <div className="row">
+          <div className="col-25">
+            <label htmlFor="state">State</label>
+          </div>
+          <div className="col-75">
+            <input type="text"  name="state" placeholder={currUser[0].state}/>
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-25">
-          <label htmlFor="email">Email</label>
+        <div className="row">
+          <div className="col-25">
+            <label htmlFor="zip">Zipcode</label>
+          </div>
+          <div className="col-75">
+          <input type="text"  name="zip" placeholder={currUser[0].zipcode}/>
+          </div>
         </div>
-        <div className="col-75">
-          <input type="text" id="lname" name="lastname" placeholder="Your email.."/>
+        <div className="row">
+          <input type="submit" value="Submit Changes"/>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-25">
-          <label htmlFor="subject">Subject</label>
-        </div>
-        <div className="col-75">
-          <textarea id="subject" name="subject" placeholder="Write something.." ></textarea>
-        </div>
-      </div>
-      <div className="row">
-        <input type="submit" value="Submit"/>
-      </div>
-    </form>
-  </div>
-  )
+      </form>
+    </div>
+    )
+  }
 }
 
 export default UserSettings
