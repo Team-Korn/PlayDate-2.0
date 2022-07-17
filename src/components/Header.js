6cb6adacf03266268828799c6b5cbb474b1018c2
Sherrina Robinson <robinsonsherrina@gmail.com>
2022-07-17 11:18:56 -0400

    added logout icon to chat header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Header.css'
+import styled from 'styled-components';
 import PersonIcon from '@material-ui/icons/Person'
 import { IconButton } from '@material-ui/core'
+import LogoutIcon from '@mui/icons-material/Logout';
 import ForumIcon from '@material-ui/icons/Forum'
 import { Link, useHistory } from 'react-router-dom'
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
+
+const StyledLogoLink = styled(Link)`
+  font-size: 2.5vw;
+  text-decoration: none;
+  font-weight: 300px;
+  color: #8fd0ff;
+  &:hover {
+    color: #white;
+  }
+`;
 
 function Header() {
   return (
@@ -12,51 +24,23 @@ function Header() {
       <IconButton>
         <PersonIcon fontSize="large" className="header__icon" />
       </IconButton>
-      <Link to='/home'>
-        <img
-          className="header__logo"
-          src="Playdate 🐾"
-          alt="Playdate 🐾"
-        />
-      </Link>
+
+      <StyledLogoLink to="/home" className="header__logo">Playdate 🐾</StyledLogoLink>
 
       <Link to='/chat'>
         <IconButton>
           <ForumIcon fontSize="large" className="header__icon" />
         </IconButton>
       </Link>
+
+      <Link to='/login'>
+        <IconButton>
+          <LogoutIcon fontSize="large" className="header__icon" />
+        </IconButton>
+      </Link>
     </div>
   );
 
-  // const history = useHistory()
-
-  //   return (
-  //     <div className='header'>
-  //       {backButton ? (
-  //         <IconButton onClick={() => history.replace(backButton)}>
-  //           <ArrowBackIosIcon fontSize='large' className='header__icon' />
-  //         </IconButton>
-  //       ) : (
-  //         <IconButton>
-  //           <PersonIcon fontSize='large' className='header__icon' />
-  //         </IconButton>
-  //       )}
-
-  //       <Link to='/'>
-  //         <img
-  //           className='header__logo'
-  //           src='https://i.dlpng.com/static/png/6184865-this-post-is-a-part-of-tinder-logo-transparent-background-hd-tinder-logo-png-840_771_preview.png'
-  //           alt='tinderLogo'
-  //         />
-  //       </Link>
-
-  //       <Link to='/chat'>
-  //         <IconButton>
-  //           <ForumIcon fontSize='large' className='header__icon' />
-  //         </IconButton>
-  //       </Link>
-  //     </div>
-  //   )
 }
 
 export default Header
