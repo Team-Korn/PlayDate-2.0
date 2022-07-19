5867f3f0b156041e02be2bb4ff4823d4ddd82135
Katie Assoian <ktassoian@gmail.com>
2022-07-19 10:57:48 -0400

    merge

--- src/components/EndOfDeck.js
+++ src/components/EndOfDeck.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import PicCarousel from './PicCarousel';
 
 const StyledMessageWrapper = styled.div`
   display: flex;
